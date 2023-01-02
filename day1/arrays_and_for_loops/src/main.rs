313d0c7dde36926402f65719d4fa763724c68503
bigpug <101833648+BigPugLabs@users.noreply.github.com>
2023-01-02 00:17:54 +0000

    arrays and for loops

--- day1/arrays_and_for_loops/src/main.rs
+++ day1/arrays_and_for_loops/src/main.rs
@@ -17,26 +17,6 @@ fn pretty_print(matrix: &[[i32; 3]; 3]) {
     }
 }
 
-// ANCHOR: tests
-#[test]
-fn test_transpose() {
-    let matrix = [
-        [101, 102, 103], //
-        [201, 202, 203],
-        [301, 302, 303],
-    ];
-    let transposed = transpose(matrix);
-    assert_eq!(
-        transposed,
-        [
-            [101, 201, 301], //
-            [102, 202, 302],
-            [103, 203, 303],
-        ]
-    );
-}
-// ANCHOR_END: tests
-
 fn main() {
     let matrix = [
         [101, 102, 103], // <-- the comment makes rustfmt add a newline
